985fe206c5fd9557eedb0ee26c2e4dce94e80f7a
nelsonle345 <nelsonle345@gmail.com>
2023-06-05 00:08:45 -0400

    Structure edited

--- routes/index.js
+++ routes/index.js
@@ -14,22 +14,22 @@ router.get('/', function(req, res, next) {
 
 /* GET About Me page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
+  res.render('about', { title: 'About Me' });
 });
 
 /* GET Contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact Us' });
+  res.render('contact', { title: 'Contact Us' });
 });
 
 /* GET Project page. */
 router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'Projects' });
+  res.render('project', { title: 'Projects' });
 });
 
 /* GET Server page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
+  res.render('service', { title: 'Services' });
 });
 
 module.exports = router;
