4e1a7a7addc22839e39192e0861502e8f4deb9da
nelsonle345 <nelsonle345@gmail.com>
2023-06-18 21:31:05 -0400

    Edited routes

--- server/routes/contacts.js
+++ server/routes/contacts.js
@@ -12,21 +12,48 @@ router.get('/', async (req, res, next) =>{
         let contactList = await contacts.find()
         console.log(contactList)
 
-        res.render('contact_data', {title: 'Business Contacts List', contactList: contactList})
+        res.render('contact/list', {title: 'Business Contacts List', contactList: contactList})
     } catch (err){
         console.error(err);
     }
 });
 
 // Get Route for displaying the Add page - CREATE Operation
+router.get('/add', async (req, res, next) =>{
+    try {
+        res.render('contact/add', {title: 'Add Business Contact'})
+    } catch (err){
+        console.error(err);
+    }
+});
 
 //Post Route for processing the Add Page - CREATE Operation
+router.post('/add', async (req, res, next) =>{
+ let newContact = new Contact({
+    "contact_name": req.body.contact_name,
+    "contact_number": req.body.contact_number,
+    "email_address" :req.body.email_address
+
+ })
+});
 
 //Get Route for displaying the Edit Page - UPDATE Operation
+router.get('/edit/:id', async (req, res, next) =>{
+ 
+});
+
 
 //Post Route for processing the Edit Page - UPDATE Operation
+router.post('/edit/:id', async (req, res, next) =>{
+ 
+});
+
 
 //Get to perform Delection - Delete Operation
+router.get('/delete/:id', async (req, res, next) =>{
+ 
+});
+
 
 
 module.exports = router; 
