d9bf7035c001defbf43c0062f614c497179e4f49
nelsonle345 <nelsonle345@gmail.com>
2023-06-18 23:10:27 -0400

    Edit the contact views/fixing the button functions

--- server/routes/contacts.js
+++ server/routes/contacts.js
@@ -2,93 +2,90 @@ let express = require('express');
 let router = express.Router();
 let mongoose = require('mongoose');
 
-//connect to our contact model
+// Connect to our contact model
 let Contact = require('../../models/contacts');
-const contacts = require('../../models/contacts');
 
 // Get route for the contact list page - READ Operation
-router.get('/', async (req, res, next) =>{
+router.get('/', async (req, res, next) => {
     try {
-        let contactList = await contacts.find()
-        console.log(contactList)
-
-        res.render('contact/list', {title: 'Business Contacts List', contactList: contactList})
-    } catch (err){
+        let contactList = await Contact.find();
+        console.log(contactList);
+        res.render('contact/list', { title: 'Business Contacts List', contactList: contactList });
+    } catch (err) {
         console.error(err);
     }
 });
 
 // Get Route for displaying the Add page - CREATE Operation
-router.get('/add', async (req, res, next) =>{
+router.get('/add', async (req, res, next) => {
     try {
-        res.render('contact/add', {title: 'Add Business Contact'})
-    } catch (err){
+        res.render('contact/add', { title: 'Add Business Contact' });
+    } catch (err) {
         console.error(err);
     }
 });
 
-//Post Route for processing the Add Page - CREATE Operation
-router.post('/add', async (req, res, next) =>{
- let newContact = new Contact({
-    "contact_name": req.body.contact_name,
-    "contact_number": req.body.contact_number,
-    "email_address" :req.body.email_address
- });
+// Post Route for processing the Add Page - CREATE Operation
+router.post('/add', async (req, res, next) => {
+    let newContact = new Contact({
+        "contact_name": req.body.contact_name,
+        "contact_number": req.body.contact_number,
+        "email_address": req.body.email_address
+    });
 
- try{
-    await newContact.save();
-    res.redirect('/contact-list');
- } catch (err){
-   console.log(err);
-   res.status(500).send(err); 
- }
+    try {
+        console.log(newContact);
+        await newContact.save();
+        res.redirect('/contact-list');
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err);
+    }
 });
 
-//Get Route for displaying the Edit Page - UPDATE Operation
-router.get('/edit/:id', async (req, res, next) =>{
+// Get Route for displaying the Edit Page - UPDATE Operation
+router.get('/edit/:id', async (req, res, next) => {
     let id = req.params.id;
 
-    try{
-        let contactToEdit = await Contact.findbyID(id);
-        res.redirect('contact/edit', {title: 'Edit Game', game: gameToEdit})
-    } catch (err){
+    try {
+        let contactToEdit = await Contact.findById(id);
+        res.render('contact/edit', { title: 'Edit Contact', contact: contactToEdit });
+    } catch (err) {
         console.log(err);
-        res.status(500).send(err); 
-      }
+        res.status(500).send(err.message);
+    }
 });
 
-//Post Route for processing the Edit Page - UPDATE Operation
-router.post('/edit/:id', async (req, res, next) =>{
+// Post Route for processing the Edit Page - UPDATE Operation
+router.post('/edit/:id', async (req, res, next) => {
     let id = req.params.id;
     let updatedContact = {
         "contact_name": req.body.contact_name,
         "contact_number": req.body.contact_number,
-        "email_address" :req.body.email_address
+        "email_address": req.body.email_address
     };
 
-    try{
-        await Contact.updateOne({_id:id}, updatedContact);
-        res.redirect('/contact-list')
-    } catch (err){
-        console.log(err);
-        res.status(500).send(err); 
-      }
-});
-
-
-//Get to perform Delection - Delete Operation
-router.get('/delete/:id', async (req, res, next) =>{
-    let id = req.params.id;
-
-    try{
-        await Contact.findbyIDandRemove(id);
+    try {
+        console.log("Updating...");
+        await Contact.updateOne({ _id: id }, updatedContact);
         res.redirect('/contact-list');
-    }catch{
+    } catch (err) {
         console.log(err);
-        res.status(500).send(err); 
+        res.status(500).send(err);
     }
 });
 
+// Get to perform Deletion - DELETE Operation
+router.get('/delete/:id', async (req, res, next) => {
+    let id = req.params.id;
 
+    try {
+        await Contact.findByIdAndRemove(id);
+        res.redirect('/contact-list');
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err);
+    }
+});
 
-module.exports = router; 
+module.exports = router;
