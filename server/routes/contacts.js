ae96e6c9d8c08c12d32e0e1d004d80db58c5349b
nelsonle345 <nelsonle345@gmail.com>
2023-06-18 21:46:44 -0400

    Update all of the routes

--- server/routes/contacts.js
+++ server/routes/contacts.js
@@ -33,25 +33,60 @@ router.post('/add', async (req, res, next) =>{
     "contact_name": req.body.contact_name,
     "contact_number": req.body.contact_number,
     "email_address" :req.body.email_address
+ });
 
- })
+ try{
+    await newContact.save();
+    res.redirect('/contact-list');
+ } catch (err){
+   console.log(err);
+   res.status(500).send(err); 
+ }
 });
 
 //Get Route for displaying the Edit Page - UPDATE Operation
 router.get('/edit/:id', async (req, res, next) =>{
- 
-});
+    let id = req.params.id;
 
+    try{
+        let contactToEdit = await Contact.findbyID(id);
+        res.redirect('contact/edit', {title: 'Edit Game', game: gameToEdit})
+    } catch (err){
+        console.log(err);
+        res.status(500).send(err); 
+      }
+});
 
 //Post Route for processing the Edit Page - UPDATE Operation
 router.post('/edit/:id', async (req, res, next) =>{
- 
+    let id = req.params.id;
+    let updatedContact = {
+        "contact_name": req.body.contact_name,
+        "contact_number": req.body.contact_number,
+        "email_address" :req.body.email_address
+    };
+
+    try{
+        await Contact.updateOne({_id:id}, updatedContact);
+        res.redirect('/contact-list')
+    } catch (err){
+        console.log(err);
+        res.status(500).send(err); 
+      }
 });
 
 
 //Get to perform Delection - Delete Operation
 router.get('/delete/:id', async (req, res, next) =>{
- 
+    let id = req.params.id;
+
+    try{
+        await Contact.findbyIDandRemove(id);
+        res.redirect('/contact-list');
+    }catch{
+        console.log(err);
+        res.status(500).send(err); 
+    }
 });
 
 
