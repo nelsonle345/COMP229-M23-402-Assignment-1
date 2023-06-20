a14d30c7c491fd95b8a82981c96887f9ead75e11
nelsonle345 <nelsonle345@gmail.com>
2023-06-19 20:19:40 -0400

    Update db.js
    update the password

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
     "URI": "mongodb://localhost/business_contact_data",
-    "URI2": "mongodb+srv://nelsonle345:<password>@cluster0.qfpeebb.mongodb.net/"
+    "URI2": "mongodb+srv://nelsonle345:Teemole@1460@cluster0.qfpeebb.mongodb.net/"
   };
