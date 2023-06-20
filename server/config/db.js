087dfe3f2ad72425816bdae37a608cc8dc57049f
nelsonle345 <nelsonle345@gmail.com>
2023-06-19 20:17:59 -0400

    add mongo atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
-module.exports =
-{
-    "URI": "mongodb://localhost/business_contact_data"
-}
+module.exports = {
+    "URI": "mongodb://localhost/business_contact_data",
+    "URI2": "mongodb+srv://nelsonle345:<password>@cluster0.qfpeebb.mongodb.net/"
+  };
